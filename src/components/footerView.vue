926d123a4479c3b25dbadc11c2840cd5d1f82955
Georginio-prod <etonameklou6@gmail.com>
2024-08-07 16:33:41 +0000

    style: add the footer card

--- src/components/footerView.vue
+++ src/components/footerView.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-        
+    <div class="flex justify-between items-center  px-40 bg-black">
+        <div class="flex flex-col gap-8 ">
+            <img src="/public/Group.png" alt="logo" class="items-center h-full pt-16 pb-20">
+            <img src="/public/Group 12.png" alt="icons" class="pb-16">
+        </div>
+
+        <div class="flex flex-col gap-5 text-white">
+            <h3> Home</h3>
+            <h3>Stories</h3>
+            <h3>Features</h3>
+            <h3>Pricing</h3>
+        </div>
+
+        <div class="flex text-white flex-col gap-24">
+            <div class="flex justify-end">
+                <router-link to="/"> GET AN INVITE</router-link>
+                <img src="/public/Group 6.png" alt="arrow" class="text-white h-full ">
+            </div>
+            <p class="mt-4 text-sm text-gray-400">Copyright 2019. All Rights Reserved</p>
+
+        </div>
     </div>
 </template>
 
